21008779aa9980cfd8feaada4e486cc180fe92b5
Jerere <jere.lampinen@myy.haaga-helia.fi>
2018-11-15 10:21:27 +0200

    fix

--- Survey/Kyselypalvelu/src/main/java/ByteMe/Kyselypalvelu/Controller/KyselypalveluController.java
+++ Survey/Kyselypalvelu/src/main/java/ByteMe/Kyselypalvelu/Controller/KyselypalveluController.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,21 +17,26 @@ import ByteMe.Kyselypalvelu.Model.Kysely;
 import ByteMe.Kyselypalvelu.Model.KyselyRepository;
 import ByteMe.Kyselypalvelu.Model.Kysymys;
 import ByteMe.Kyselypalvelu.Model.KysymysRepository;
+import ByteMe.Kyselypalvelu.Model.Vastaus;
+import ByteMe.Kyselypalvelu.Model.VastausRepository;
 
 @Controller
+@CrossOrigin
 public class KyselypalveluController {
 
 	@Autowired
 	private KyselyRepository kyselyrepository;
 	@Autowired
 	private KysymysRepository kysymysrepository;
-	
-	//uudelleenohjaa 
-//	@RequestMapping("/")
-//	public String index() {
-//		return "redirect:/kysymykset";
-//	}
-	
+	@Autowired
+	private VastausRepository vastausrepository;
+
+	// uudelleenohjaa
+	// @RequestMapping("/")
+	// public String index() {
+	// return "redirect:/kysymykset";
+	// }
+
 	// PALAUTTAA KAIKKI KYSYMYKSET
 	@RequestMapping("/kysymykset")
 	public @ResponseBody List<Kysymys> findKyselyRest() {
@@ -53,11 +60,19 @@ public class KyselypalveluController {
 	public @ResponseBody Optional<Kysely> findKyselyRest(@PathVariable("id") Long kyselyId) {
 		return kyselyrepository.findById(kyselyId);
 	}
-	
+
 	// LISÄÄ KYSYMYKSEN
 	@PostMapping("/uusiKysymys")
-	public String helloYou(@RequestParam(name="firstname") String etunimi, 
-			@RequestParam(name="lastname") String sukunimi) {
-		return "Hello " + etunimi + " " +sukunimi;
-	} 
+	public String helloYou(@RequestParam(name = "firstname") String etunimi,
+			@RequestParam(name = "lastname") String sukunimi) {
+		return "Hello " + etunimi + " " + sukunimi;
 	}
+	
+	// UUSI VASTAUS
+	@PostMapping("/uusiVastaus")
+	Vastaus uusiVastaus(@RequestBody Vastaus vastaus) {
+		return vastausrepository.save(vastaus);
+		
+	}
+	
+}
